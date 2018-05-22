1458bffe54fca97397de7f29ee8760ddf395d454
Connor Poole <connor.poole@ring.com>
2018-05-21 18:00:55 -0700

    added jump host sgs

--- modules/base-vpc/security_groups.tf
+++ modules/base-vpc/security_groups.tf
@@ -53,3 +53,59 @@ resource "aws_security_group" "allow_all_within_vpc" {
 output "aws_security_group.allow_all_within_vpc.id" {
   value = "${aws_security_group.allow_all_within_vpc.id}"
 }
+
+#########################################################################
+resource "aws_security_group" "qualys_sg" {
+  name   = "qualys-sg"
+  vpc_id = "${var.vpc_id}"
+
+  ingress {
+    from_port = 0
+    to_port   = 0
+    protocol  = "-1"
+    self      = true
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+output "aws_security_group.qualys_sg.id" {
+  value = "${aws_security_group.qualys_sg.id}"
+}
+
+#######################################################################
+resource "aws_security_group" "ring_jumphost_sg" {
+  name   = "ring-jumphost"
+  vpc_id = "${var.vpc_id}"
+
+  ingress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    security_groups = ["${aws_security_group.qualys_sg.id}"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+output "aws_security_group.ring_jumphost_sg.id" {
+  value = "${aws_security_group.ring_jumphost_sg.id}"
+}
+
