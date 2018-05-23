8d2e8527d20bb37fdc488d7750f36ecc34bbcf56
Brian Tai <brian.tai@ring.com>
2018-05-22 18:10:20 -0700

    change bucket name for tf states for new dev account

--- implementations/buckets/terraform-bucket/bucket.tf
+++ implementations/buckets/terraform-bucket/bucket.tf
@@ -1,7 +1,7 @@
-resource "aws_s3_bucket" "ring-terraform-practice" {
+resource "aws_s3_bucket" "ring-terraform-states-practice" {
   provider = "aws.dev"
-  acl = "private"
-  bucket = "ring-terraform-practice"
+  acl      = "private"
+  bucket   = "ring-terraform-states-practice"
 
   versioning {
     enabled = "true"
