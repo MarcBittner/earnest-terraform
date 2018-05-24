3a9a8df16ab9f52af0378d11db7b7da3a0dd509a
Brian Tai <brian.tai@ring.com>
2018-05-23 17:25:44 -0700

    add env and region outputs for generic data

--- modules/generic-data/outputs.tf
+++ modules/generic-data/outputs.tf
@@ -9,3 +9,11 @@ output "environment-region-mapping" {
 output "environment-short-to-long-name-mapping" {
   value = "${var.environment-short-to-long-name-mapping}"
 }
+
+output "env" {
+  value = "${element(split("-", terraform.workspace), 0)}"
+}
+
+output "region" {
+  value = "${join("-", slice(split("-", terraform.workspace), 1, length(split("-", terraform.workspace))))}"
+}
