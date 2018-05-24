c69ccc079d76e87f9030c776c01ea2b8050a45da
Brian Tai <brian.tai@ring.com>
2018-05-23 20:34:12 -0700

    comment out redis output endpoint for now

--- modules/redis-cluster/outputs.tf
+++ modules/redis-cluster/outputs.tf
@@ -1,3 +1,4 @@
-output "redis_endpoint" {
-  value = "${aws_elasticache_replication_group.redis.primary_endpoint_address}"
-}
+# output "redis_endpoint" {
+#   value = "${aws_elasticache_replication_group.redis.primary_endpoint_address}"
+# }
+
