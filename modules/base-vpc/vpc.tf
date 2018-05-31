96edcb4c68e6ffc7a1ce7dae33ebcf5765ec6eaa
Brian Tai <brian.tai@ring.com>
2018-05-31 14:48:03 -0700

    add name tag to vpc

--- modules/base-vpc/vpc.tf
+++ modules/base-vpc/vpc.tf
@@ -5,10 +5,14 @@ resource "aws_vpc" "this" {
   cidr_block           = "${var.cidr_base}.0.0/16"
   enable_dns_hostnames = "true"
   enable_dns_support   = "true"
+
+  tags {
+    Name = "${var.name}"
+  }
 }
 
 #Outputs
-output "vpc.id" {
+w output "vpc.id" {
   value = "${aws_vpc.this.id}"
 }
 
