94a956e8506b3aca90be365e300109145c8b46d3
Corey Gale <corey.gale@ring.com>
2018-06-01 14:26:19 -0700

    Add common ELB module

---
+++ modules/classic-load-balancer/elb.tf
@@ -0,0 +1,30 @@
+resource "aws_elb" "elb" {
+  name                      = "${var.name}"
+  security_groups           = ["${var.security_groups}"]
+  subnets                   = ["${var.subnets}"]
+  cross_zone_load_balancing = true
+
+  access_logs {
+    bucket        = "${var.logging_bucket}"
+    bucket_prefix = "${var.region}/${var.environment}/${var.name}"
+    interval      = 5
+  }
+
+  internal                    = "${var.internal_load_balancer}"
+  listener                    = "${var.listeners}"
+  idle_timeout                = "${var.idle_timeout}"
+  connection_draining         = "${var.connection_draining}"
+  connection_draining_timeout = "${var.connection_draining_timeout}"
+
+  health_check {
+    healthy_threshold   = "${var.healthy_threshold}"
+    unhealthy_threshold = "${var.unhealthy_threshold}"
+    timeout             = "${var.timeout}"
+    target              = "${var.target}"
+    interval            = "${var.interval}"
+  }
+
+  tags {
+    Name = "${var.name}"
+  }
+}
---
+++ modules/classic-load-balancer/variables.tf
@@ -0,0 +1,55 @@
+variable "name" {}
+
+variable "region" {}
+
+variable "environment" {}
+
+variable "security_groups" {
+  type = "list"
+}
+
+variable "subnets" {
+  type = "list"
+}
+
+variable "listeners" {
+  type = "list"
+}
+
+variable "idle_timeout" {
+  default = 400
+}
+
+variable "connection_draining" {
+  default = true
+}
+
+variable "connection_draining_timeout" {
+  default = 400
+}
+
+variable "healthy_threshold" {
+  default = 10
+}
+
+variable "unhealthy_threshold" {
+  default = 2
+}
+
+variable "timeout" {
+  default = 5
+}
+
+variable "target" {}
+
+variable "interval" {
+  default = 30
+}
+
+variable "internal_load_balancer" {
+  default = true
+}
+
+variable "logging_bucket" {
+  description = "The S3 bucket to send access logs to"
+}
