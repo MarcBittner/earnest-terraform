3d5b13b4b1ff12477a5bc6b3d6f6e13f784ea7af
Brian Tai <brian.tai@ring.com>
2018-06-01 16:52:25 -0700

    initial dynamodb module

---
+++ modules/dynamo/table.tf
@@ -0,0 +1,39 @@
+locals {
+  attributes = [{
+    name = "${var.hash_key}"
+    type = "S"
+  },
+    {
+      name = "${var.range_key}"
+      type = "S"
+    },
+    "${var.additional_attributes}",
+  ]
+}
+
+resource "aws_dynamodb_table" "basic-dynamodb-table" {
+  name           = "${var.name}"
+  read_capacity  = "${var.read_capacity}"
+  write_capacity = "${var.write_capacity}"
+  hash_key       = "${var.hash_key}"
+  range_key      = "${var.range_key}"
+
+  attribute = ["${var.attributes}"]
+
+  server_side_encryption {
+    enabled = "${var.enable_encryption}"
+  }
+
+  ttl {
+    attribute_name = "${var.ttl_attribute_name}"
+    enabled        = "${var.ttl_enabled}"
+  }
+
+  global_secondary_index = ["${var.global_secondary_indexes}"]
+  local_secondary_index  = ["${var.local_secondary_indexes}"]
+
+  tags {
+    Name        = "${var.name}-${module.generic-data.env}"
+    Environment = "${module.generic-data.env}"
+  }
+}
---
+++ modules/dynamo/variables.tf
@@ -0,0 +1,52 @@
+variable "name" {
+  description = "The name of the dynamo db table"
+}
+
+variable "read_capacity" {
+  description = "The read capacity of the dynamo db table"
+}
+
+variable "write_capacity" {
+  description = "The write capacity of the dynamo db table"
+}
+
+variable "hash_key" {
+  description = "The partition key that is used to partition data"
+}
+
+variable "range_key" {
+  description = "The sort key used for searching within a partition"
+}
+
+variable "enabled_encryption" {
+  description = "Whether encryption is enabled"
+  default     = false
+}
+
+variable "additional_attributes" {
+  description = "List of additional table attributes and their types"
+  type        = "list"
+  default     = []
+}
+
+variable "ttl_attribute_name" {
+  description = "The attribute_name used for the TTL i.e. created_at"
+  default     = ""
+}
+
+variable "ttl_enabled" {
+  description = "Whether the ttl is enabled"
+  default     = false
+}
+
+variable "global_secondary_indexes" {
+  type        = "list"
+  default     = []
+  description = "List of global secondary indexes"
+}
+
+variable "local_secondary_indexes" {
+  type        = "list"
+  default     = []
+  description = "List of local secondary indexes"
+}
