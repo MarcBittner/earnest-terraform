3fec1b1f4885c45265f3cc68e516621a4de06988
Connor Poole <connor.poole@ring.com>
2018-06-13 10:15:43 -0700

    added foreign peering to old prod

---
+++ implementations/legacy-vpc-peering/variables.tf
@@ -0,0 +1,11 @@
+variable "old-prod-public-route-tables" {
+  default = ["blah", "blah"]
+}
+
+variable "old-prod-private-route-tables" {
+  default = ["blah", "blah"]
+}
+
+varialbe "old-prod-cidr" {
+  default = "x.x.x.x/16"
+}
