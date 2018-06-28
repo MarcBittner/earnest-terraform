fddeb462ab9bf2483bf321f6f1e570a8f350bc0f
Brian Tai <brian.tai@ring.com>
2018-06-28 13:13:34 -0700

    fix iam naming so service names follow this pattern

--- modules/iam/iam.tf
+++ modules/iam/iam.tf
@@ -1,16 +1,16 @@
 resource "aws_iam_instance_profile" "default_instance_profile" {
-  name_prefix = "${substr(format("%s%sProfile", join("",split(" ",var.name)), title(var.environment)), 0, 32)}"
+  name_prefix = "${substr(format("%s%sProfile", join("",split("-",var.name)), title(var.environment)), 0, 32)}"
   role        = "${aws_iam_role.default_instance_role.name}"
 }
 
 resource "aws_iam_role" "default_instance_role" {
-  name_prefix = "${substr(format("%s%sRole", join("",split(" ",var.name)), title(var.environment)), 0 ,32)}"
+  name_prefix = "${substr(format("%s%sRole", join("",split("-",var.name)), title(var.environment)), 0 ,32)}"
 
   assume_role_policy = "${var.assume_role_policy}"
 }
 
 resource "aws_iam_role_policy" "instance_role_policy" {
-  name_prefix = "${substr(format("%s%sRolePolicy", join("",split(" ",var.name)), title(var.environment)), 0, 32)}"
+  name_prefix = "${substr(format("%s%sRolePolicy", join("",split("-",var.name)), title(var.environment)), 0, 32)}"
   role        = "${aws_iam_role.default_instance_role.id}"
 
   policy = "${var.role_policy}"
