1ecd7b87e2b7155c98526b4be5e288c6884d9074
Brian Tai <brian.tai@ring.com>
2018-06-28 15:54:38 -0700

    fix naming on aim to be

--- modules/iam/iam.tf
+++ modules/iam/iam.tf
@@ -1,16 +1,16 @@
 resource "aws_iam_instance_profile" "default_instance_profile" {
-  name_prefix = "${format("%s%sProfile", join("",split("-",var.name)), title(var.environment))}"
+  name_prefix = "${format("%s-%s-profile", var.name, var.environment)}"
   role        = "${aws_iam_role.default_instance_role.name}"
 }
 
 resource "aws_iam_role" "default_instance_role" {
-  name_prefix = "${format("%s%sRole", join("",split("-",var.name)), title(var.environment))}"
+  name_prefix = "${format("%s-%s-role", var.name, var.environment)}"
 
   assume_role_policy = "${var.assume_role_policy}"
 }
 
 resource "aws_iam_role_policy" "instance_role_policy" {
-  name_prefix = "${format("%s%sRolePolicy", join("",split("-",var.name)), title(var.environment))}"
+  name_prefix = "${format("%s-%s-role-policy", var.name, var.environment)}"
   role        = "${aws_iam_role.default_instance_role.id}"
 
   policy = "${var.role_policy}"
