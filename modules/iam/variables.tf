4d818a0b58c98726839e5dd5668ff93c855e4b7c
Brian Tai <brian.tai@ring.com>
2018-06-28 15:45:39 -0700

    add iam variables that were missing

--- modules/iam/variables.tf
+++ modules/iam/variables.tf
@@ -5,3 +5,11 @@ variable "name" {
 variable "environment" {
   description = "The environment to be deployed to"
 }
+
+variable "role_policy" {
+  description = "The policy to be attached to the role"
+}
+
+variable "assume_role_policy" {
+  description = "The assume role policy to be attached to the role"
+}
