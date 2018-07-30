0c7c51965aef607df7c4ae680ead6429f5a5d7b7
Brian Tai <brian.tai@ring.com>
2018-07-30 15:53:22 -0700

    specify role arn and instance arn as output

--- modules/iam/outputs.tf
+++ modules/iam/outputs.tf
@@ -1,3 +1,7 @@
-output "arn" {
+output "role_arn" {
+  value = "${aws_iam_role.default_instance_role.arn}"
+}
+
+output "instance_profile_arn" {
   value = "${aws_iam_instance_profile.default_instance_profile.arn}"
 }
