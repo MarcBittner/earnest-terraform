c24c733cb89b58d89c1769ca245797f3f5d75504
Brian Tai <brian.tai@ring.com>
2018-07-30 15:44:39 -0700

    dont use name_prefix for iam anymore

--- modules/iam/iam.tf
+++ modules/iam/iam.tf
@@ -1,17 +1,17 @@
 resource "aws_iam_instance_profile" "default_instance_profile" {
-  name_prefix = "${format("%s-%s-profile", var.name, var.environment)}"
-  role        = "${aws_iam_role.default_instance_role.name}"
+  name = "${format("%s-%s-profile", var.name, var.environment)}"
+  role = "${aws_iam_role.default_instance_role.name}"
 }
 
 resource "aws_iam_role" "default_instance_role" {
-  name_prefix = "${format("%s-%s-role", var.name, var.environment)}"
+  name = "${format("%s-%s-role", var.name, var.environment)}"
 
   assume_role_policy = "${var.assume_role_policy}"
 }
 
 resource "aws_iam_role_policy" "instance_role_policy" {
-  name_prefix = "${format("%s-%s-role-policy", var.name, var.environment)}"
-  role        = "${aws_iam_role.default_instance_role.id}"
+  name = "${format("%s-%s-role-policy", var.name, var.environment)}"
+  role = "${aws_iam_role.default_instance_role.id}"
 
   policy = "${var.role_policy}"
 }
