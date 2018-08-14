46f905053737cd68879da38cc7d8b6c31ac8ccd5
JonLittleIT <jonathantlittleit@gmail.com>
2018-08-13 17:46:25 -0700

    Kenisis stream
    adding Kenisis stream to aws-sec account.

---
+++ Kenisis-stream.tf
@@ -0,0 +1,19 @@
+#### Kenisis stream setup Terraform
+#### We are going to add a HEC token and a Port open
+
+resource "aws_kinesis_stream" "test_stream" {
+  name             = "terraform-kinesis-aws-sec"
+  shard_count      = 1
+  retention_period = 48
+
+  shard_level_metrics = [
+    "IncomingBytes",
+    "OutgoingBytes",
+  ]
+
+  tags {
+    Environment = "security"
+  }
+}
+»
+
