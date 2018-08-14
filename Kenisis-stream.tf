09e8625804b8b855ddb3687b6a64ae56bf041500
JonLittleIT <jonathantlittleit@gmail.com>
2018-08-13 18:02:32 -0700

    added stream name
    we have a name but was not exact stream name and set shards.

--- Kenisis-stream.tf
+++ Kenisis-stream.tf
@@ -2,8 +2,8 @@
 #### We are going to add a HEC token and a Port open
 
 resource "aws_kinesis_stream" "test_stream" {
-  name             = "terraform-kinesis-aws-sec"
-  shard_count      = 1
+  name             = "splunk-stream"
+  shard_count      = 100
   retention_period = 48
 
   shard_level_metrics = [
